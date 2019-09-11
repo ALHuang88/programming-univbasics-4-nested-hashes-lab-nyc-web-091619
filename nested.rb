ec40bf7f1c58b7f415c9c498708c4982e91a9c9c
Allen Huang <alhuang1288@gmail.com>
2019-09-11 19:46:04 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -42,6 +42,10 @@ end
 def dennis_ritchies_language
 	programmer_hash = 
  		{
+ 		  :yukihiro_matsumoto => {
+      :known_for => "Ruby",
+      :languages => ["LISP", "C"]
+        }
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
